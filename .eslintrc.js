b9be585f87990e2e01d3bcbf23fa3f5c44b4581e
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-01-24 01:18:03 -0300

    feat: add eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,65 @@
-/** @type {import('eslint').Linter.Config} */
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-};
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    camelcase: "error",
+    "use-isnan": "error",
+    "no-unreachable": "error",
+    "import/no-unresolved": "off",
+    "newline-before-return": "error",
+    "no-useless-computed-key": "error",
+    eqeqeq: [
+      "error",
+      "always",
+      {
+        null: "ignore",
+      },
+    ],
+    "one-var": [
+      "error",
+      {
+        initialized: "never",
+      },
+    ],
+    "prefer-destructuring": [
+      "error",
+      {
+        array: true,
+        object: true,
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "return",
+      },
+      {
+        blankLine: "always",
+        prev: "multiline-block-like",
+        next: "if",
+      },
+      {
+        blankLine: "always",
+        prev: "multiline-block-like",
+        next: "expression",
+      },
+      {
+        blankLine: "always",
+        prev: "const",
+        next: "if",
+      },
+    ],
+  },
+}
