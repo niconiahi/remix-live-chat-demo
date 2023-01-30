0ac619e1b0de96d8a5f0cc946ce7664828fb5884
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-01-30 11:01:08 -0300

    feat: change repo type to esm + rename files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ["./app/**/*.{ts,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
@@ -21,4 +21,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
